320632c2a1d5f0f29e61172e6928eb1c8fb0b921
Michael Kofi Badu <mbadukofi@gmail.com>
2025-09-08 21:27:47 -0400

    Add ML Analysis

--- spx_analysis.py
+++ spx_analysis.py
@@ -9,3 +9,21 @@ print(df.describe())
 df['Date'] = pd.to_datetime(df['Date'])
 print(df.info())
 print(df.loc[df['Date'].dt.year == 2025])
+print(df.groupby(df['Date'].dt.year)[['SPX','GLD']].mean())
+print(df.groupby(df['Date'].dt.year)[['SPX','GLD']].count())
+df = df.set_index('Date')
+print(df.head())
+print(df.corr())
+print(sns.heatmap(df.corr(), cmap="coolwarm"))
+print(df['SPX'].plot())
+from sklearn.linear_model import LinearRegression
+lr=LinearRegression()
+y = df['SPX']
+x = df[['GLD', 'SLV']]
+lr.fit(x,y)
+predict = lr.predict(x)
+sns.lineplot(data=df,y='SPX',x=df.index)
+sns.lineplot(y=predict,x=df.index)
+from sklearn.metrics import mean_absolute_error,mean_absolute_percentage_error
+print(mean_absolute_error(y,predict))
+print(mean_absolute_percentage_error(y,predict))
