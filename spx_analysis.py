4eb41d342e082e61c9f26094f52b833c97f52e10
Michael Kofi Badu <mbadukofi@gmail.com>
2025-09-10 17:08:07 -0400

    add commentss

--- spx_analysis.py
+++ spx_analysis.py
@@ -15,20 +15,24 @@ print(df.groupby(df['Date'].dt.year)[['SPX','GLD']].mean())
 print(df.groupby(df['Date'].dt.year)[['SPX','GLD']].count())
 df = df.set_index('Date')
 print(df.head())
-#read the header of the row
+# Compute and print correlation matrix between variables
 print(df.corr())
 print(sns.heatmap(df.corr(), cmap="coolwarm"))
 print(df['SPX'].plot())
 from sklearn.linear_model import LinearRegression
 #Looking into the regression of the data
 lr=LinearRegression()
+# Define target variable (y) and features (x)
 y = df['SPX']
 x = df[['GLD', 'SLV']]
+# Train the regression model
 lr.fit(x,y)
+# Predict SPX values using trained model
 predict = lr.predict(x)
 sns.lineplot(data=df,y='SPX',x=df.index)
+# Plot predicted SPX values over time
 sns.lineplot(y=predict,x=df.index)
 from sklearn.metrics import mean_absolute_error,mean_absolute_percentage_error
-#Final Output
+# Evaluate model performance with error metrics
 print(mean_absolute_error(y,predict))
 print(mean_absolute_percentage_error(y,predict))
